412c2cc3de1ddd319f08184dbf5c41f546edc88b
johnzdictator <yohannesketemazeleke@gmail.com>
2021-03-04 08:49:44 +0300

    club controller commit

--- lib/club/controller/club_controller.dart
+++ lib/club/controller/club_controller.dart
@@ -6,17 +6,43 @@ class ClubController extends ResourceController {
   final ManagedContext context;
 
   @Operation.get()
-  Future<Response> getAllClubs() async {}
+  Future<Response> getAllClubs() async {
+    final clubQuery = Query<Club>(context);
+    final clubs = await clubQuery.fetch();
+    return Response.ok(clubs);
+  }
 
   @Operation.get('id')
-  Future<Response> getClubById(@Bind.path('id') int id) async {}
+  Future<Response> getClubById(@Bind.path('id') int id) async {
+    final clubQuery = Query<Club>(context)..where((c) => c.id).equalTo(id);
+    final club = await clubQuery.fetchOne();
+    if (club == null) {
+      return Response.notFound();
+    }
+    return Response.ok(club);
+  }
 
   @Operation.post()
-  Future<Response> postClub(@Bind.body(ignore: ['id']) Club club) async {}
+  Future<Response> postClub(@Bind.body(ignore: ['id']) Club club) async {
+    final clubQuery = Query<Club>(context)..values = club;
+    final insertClub = await clubQuery.insert();
+    return Response.ok(insertClub);
+  }
 
   @Operation.put('id')
-  Future<Response> updateClub(@Bind.path('id') int id) async {}
+  Future<Response> updateClub(
+      @Bind.path('id') int id, @Bind.body(ignore: ['id']) Club club) async {
+    final clubQuery = Query<Club>(context)
+      ..where((c) => c.id).equalTo(id)
+      ..values = club;
+    final editClub = await clubQuery.update();
+    return Response.ok(editClub);
+  }
 
   @Operation.delete('id')
-  Future<Response> deleteClub(@Bind.path('id') int id) async {}
+  Future<Response> deleteClub(@Bind.path('id') int id) async {
+    final clubQuery = Query<Club>(context)..where((c) => c.id).equalTo(id);
+    final removeClub = await clubQuery.delete();
+    return Response.ok(removeClub);
+  }
 }
