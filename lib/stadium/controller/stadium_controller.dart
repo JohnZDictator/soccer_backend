1b3ff0ef41cef950ae9cd9c356afccee230bd18a
johnzdictator <yohannesketemazeleke@gmail.com>
2021-03-04 09:00:57 +0300

    stadium controller

--- lib/stadium/controller/stadium_controller.dart
+++ lib/stadium/controller/stadium_controller.dart
@@ -6,18 +6,46 @@ class StadiumController extends ResourceController {
   final ManagedContext context;
 
   @Operation.get()
-  Future<Response> getAllStadiums() async {}
+  Future<Response> getAllStadiums() async {
+    final stadiumQuery = Query<Stadium>(context);
+    final stadiums = stadiumQuery.fetch();
+    return Response.ok(stadiums);
+  }
 
   @Operation.get('id')
-  Future<Response> getStadiumById(@Bind.path('id') int id) async {}
+  Future<Response> getStadiumById(@Bind.path('id') int id) async {
+    final stadiumQuery = Query<Stadium>(context)
+      ..where((s) => s.id).equalTo(id);
+    final stadium = stadiumQuery.fetchOne();
+    if (stadium == null) {
+      return Response.notFound();
+    }
+    return Response.ok(stadium);
+  }
 
   @Operation.post()
   Future<Response> postStadium(
-      @Bind.body(ignore: ['id']) Stadium stadium) async {}
+      @Bind.body(ignore: ['id']) Stadium stadium) async {
+    final stadiumQuery = Query<Stadium>(context)..values = stadium;
+    final insertStadium = stadiumQuery.insert();
+    return Response.ok(insertStadium);
+  }
 
   @Operation.put('id')
-  Future<Response> updateStadium(@Bind.path('id') int id) async {}
+  Future<Response> updateStadium(@Bind.path('id') int id,
+      @Bind.body(ignore: ['id']) Stadium stadium) async {
+    final stadiumQuery = Query<Stadium>(context)
+      ..where((s) => s.id).equalTo(id)
+      ..values = stadium;
+    final editStadium = stadiumQuery.update();
+    return Response.ok(editStadium);
+  }
 
   @Operation.delete('id')
-  Future<Response> deleteStadium(@Bind.path('id') int id) async {}
+  Future<Response> deleteStadium(@Bind.path('id') int id) async {
+    final stadiumQuery = Query<Stadium>(context)
+      ..where((s) => s.id).equalTo(id);
+    final removeStadium = stadiumQuery.delete();
+    return Response.ok(removeStadium);
+  }
 }
