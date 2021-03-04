606c4e67a39e43333475c86b90afc0c8936c69da
johnzdictator <yohannesketemazeleke@gmail.com>
2021-03-04 08:59:51 +0300

    rank_club controller

--- lib/rank_club/controller/rank_club_controller.dart
+++ lib/rank_club/controller/rank_club_controller.dart
@@ -6,18 +6,43 @@ class RankClubController extends ResourceController {
   final ManagedContext context;
 
   @Operation.get()
-  Future<Response> getAllClubRanks() async {}
+  Future<Response> getAllClubRanks() async {
+    final rankQuery = Query<RankClub>(context);
+    final ranks = rankQuery.fetch();
+    return Response.ok(ranks);
+  }
 
   @Operation.get('id')
-  Future<Response> getClubRankById(@Bind.path('id') int id) async {}
+  Future<Response> getClubRankById(@Bind.path('id') int id) async {
+    final rankQuery = Query<RankClub>(context)
+      ..where((cr) => cr.id).equalTo(id);
+    final rank = rankQuery.fetchOne();
+    return Response.ok(rank);
+  }
 
   @Operation.post()
   Future<Response> postClubRank(
-      @Bind.body(ignore: ['id']) RankClub rankClub) async {}
+      @Bind.body(ignore: ['id']) RankClub rankClub) async {
+    final rankQuery = Query<RankClub>(context)..values = rankClub;
+    final insertRank = rankQuery.insert();
+    return Response.ok(insertRank);
+  }
 
   @Operation.put('id')
-  Future<Response> updateClubRank(@Bind.path('id') int id) async {}
+  Future<Response> updateClubRank(@Bind.path('id') int id,
+      @Bind.body(ignore: ['id']) RankClub rankClub) async {
+    final rankQuery = Query<RankClub>(context)
+      ..where((cr) => cr.id).equalTo(id)
+      ..values = rankClub;
+    final editRank = rankQuery.update();
+    return Response.ok(editRank);
+  }
 
   @Operation.delete('id')
-  Future<Response> deleteClubRank(@Bind.path('id') int id) async {}
+  Future<Response> deleteClubRank(@Bind.path('id') int id) async {
+    final rankQuery = Query<RankClub>(context)
+      ..where((cr) => cr.id).equalTo(id);
+    final removeRank = rankQuery.delete();
+    return Response.ok(removeRank);
+  }
 }
