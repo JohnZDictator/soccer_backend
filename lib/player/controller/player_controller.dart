197d17691d0a0381e526cf1ae4f81ad220b5a226
johnzdictator <yohannesketemazeleke@gmail.com>
2021-03-04 08:58:09 +0300

    player controller

--- lib/player/controller/player_controller.dart
+++ lib/player/controller/player_controller.dart
@@ -6,17 +6,43 @@ class PlayerController extends ResourceController {
   final ManagedContext context;
 
   @Operation.get()
-  Future<Response> getAllPlayers() async {}
+  Future<Response> getAllPlayers() async {
+    final playerQuery = Query<Player>(context);
+    final players = playerQuery.fetch();
+    return Response.ok(players);
+  }
 
   @Operation.get('id')
-  Future<Response> getPlayerById(@Bind.path('id') int id) async {}
+  Future<Response> getPlayerById(@Bind.path('id') int id) async {
+    final playerQuery = Query<Player>(context)..where((p) => p.id).equalTo(id);
+    final player = playerQuery.fetchOne();
+    if (player == null) {
+      return Response.notFound();
+    }
+    return Response.ok(player);
+  }
 
   @Operation.post()
-  Future<Response> postPlayer(@Bind.body(ignore: ['id']) Player player) async {}
+  Future<Response> postPlayer(@Bind.body(ignore: ['id']) Player player) async {
+    final playerQuery = Query<Player>(context)..values = player;
+    final insertPlayer = playerQuery.insert();
+    return Response.ok(insertPlayer);
+  }
 
   @Operation.put('id')
-  Future<Response> updatePlayer(@Bind.path('id') int id) async {}
+  Future<Response> updatePlayer(
+      @Bind.path('id') int id, @Bind.body(ignore: ['id']) Player player) async {
+    final playerQuery = Query<Player>(context)
+      ..where((p) => p.id).equalTo(id)
+      ..values = player;
+    final editPlayer = playerQuery.update();
+    return Response.ok(editPlayer);
+  }
 
   @Operation.delete('id')
-  Future<Response> deletePlayer(@Bind.path('id') int id) async {}
+  Future<Response> deletePlayer(@Bind.path('id') int id) async {
+    final playerQuery = Query<Player>(context)..where((p) => p.id).equalTo(id);
+    final removePlayer = playerQuery.delete();
+    return Response.ok(removePlayer);
+  }
 }
