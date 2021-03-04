3a365629beadb1b86c4ad92d3c1ea4de1171bcf7
johnzdictator <yohannesketemazeleke@gmail.com>
2021-03-04 08:52:57 +0300

    game_analysis controller commit

--- lib/game_analysis/controller/game_analysis_controller.dart
+++ lib/game_analysis/controller/game_analysis_controller.dart
@@ -6,18 +6,46 @@ class GameAnalysisController extends ResourceController {
   final ManagedContext context;
 
   @Operation.get()
-  Future<Response> getAllGameAnalysis() async {}
+  Future<Response> getAllGameAnalysis() async {
+    final analysisQuery = Query<GameAnalysis>(context);
+    final analysis = await analysisQuery.fetch();
+    return Response.ok(analysis);
+  }
 
   @Operation.get('id')
-  Future<Response> getGameAnalysisById(@Bind.path('id') int id) async {}
+  Future<Response> getGameAnalysisById(@Bind.path('id') int id) async {
+    final analysisQuery = Query<GameAnalysis>(context)
+      ..where((g) => g.id).equalTo(id);
+    final analysis = await analysisQuery.fetchOne();
+    if (analysis == null) {
+      return Response.notFound();
+    }
+    return Response.ok(analysis);
+  }
 
   @Operation.post()
   Future<Response> postGameAnalysis(
-      @Bind.body(ignore: ['id']) GameAnalysis gameAnalysis) async {}
+      @Bind.body(ignore: ['id']) GameAnalysis gameAnalysis) async {
+    final analysisQuery = Query<GameAnalysis>(context)..values = gameAnalysis;
+    final insertAnalysis = await analysisQuery.insert();
+    return Response.ok(insertAnalysis);
+  }
 
   @Operation.put('id')
-  Future<Response> updateGameAnalysis(@Bind.path('id') int id) async {}
+  Future<Response> updateGameAnalysis(@Bind.path('id') int id,
+      @Bind.body(ignore: ['id']) GameAnalysis gameAnalysis) async {
+    final analysisQuery = Query<GameAnalysis>(context)
+      ..where((g) => g.id).equalTo(id)
+      ..values = gameAnalysis;
+    final editAnalysis = await analysisQuery.update();
+    return Response.ok(editAnalysis);
+  }
 
   @Operation.delete('id')
-  Future<Response> deleteGameAnalysis(@Bind.path('id') int id) async {}
+  Future<Response> deleteGameAnalysis(@Bind.path('id') int id) async {
+    final analysisQuery = Query<GameAnalysis>(context)
+      ..where((g) => g.id).equalTo(id);
+    final removeAnalysis = await analysisQuery.delete();
+    return Response.ok(removeAnalysis);
+  }
 }
